3468c7f12eb8684146b448840491be297311cbfb
Pablo <pashalennik@gmail.com>
2018-09-20 00:00:07 +0300

    123

---
+++ src/ArrayStorage.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class ArrayStorage {
+        Resume[] storage = new Resume[10000];
+            int size = 0;
+
+        void clear() {
+             Arrays.fill(storage, 0, size, null);
+                 size = 0;
+    }
+
+        void save(Resume resume) {
+               if (size < storage.length) {
+                    storage[size] = resume;
+                        size++;
+        }
+    }
+
+         Resume get(String uuid) {
+              for (int i = 0; i < size; i++) {
+                 if (storage[i].uuid == uuid) {
+                     return storage[i];
+            }
+        }
+        return null;
+    }
+
+         void delete(String uuid) {
+               for (int i = 0; i < size; i++) {
+
+                     if (storage[i].uuid == uuid) {
+                        storage[i] = storage[size - 1];
+            }
+        }
+        size--;
+    }
+
+        Resume[] getAll() {
+        Resume[] storage2 = new Resume[size];
+             return Arrays.copyOf(storage, size);
+    }
+        int size() {
+        return size;
+    }
+}
+
