013e37bccc62c7b26ddbe5e89ad1239237f405cb
Paul <paveltitan18@gmail.com>
2018-09-24 00:14:54 +0300

    UPDATE

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,7 +7,7 @@ public class ArrayStorage {
 
     void clear() {
         for (int i = 0; i < sizeOfResume; i++) {
-            storage[i] = null;
+            storage[size-1] = null;
         }
         sizeOfResume = 0;
     }
@@ -15,48 +15,42 @@ public class ArrayStorage {
     void save(Resume r) {
         for (int i = 0; i < sizeOfResume; i++) {
             if (storage[i].uuid.equals(r.uuid)) {
-                System.out.println("Resume" + r.uuid + " Putted");
+                System.out.println("Resume" + r.uuid + "Putted");
                 return;
             }
         }
         if (sizeOfResume < storage.length) {
             storage[sizeOfResume] = r;
             sizeOfResume++;
-            return;
+        } else if (sizeOfResume == storage.length) {
+            System.out.println("Full Array");
         }
-        if (sizeOfResume == storage.length) {
-            System.out.println("Full Arrays");
-            return;
-        }
+    }
+
+     void update(Resume r) {
+        get([]) = r;
+        System.out.println("Resume" + r.uuid + "Changed to" + r.uuid);
     }
 
     Resume get(String uuid) {
         for (int i = 0; i < sizeOfResume; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
+           if (storage[i].uuid.equals(uuid)) {
+                return storage[size-1] = null;
             }
         }
+        System.out.println("Resume" + uuid + "There is not");
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < sizeOfResume; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[sizeOfResume - 1];
-                storage[sizeOfResume - 1] = null;
-                sizeOfResume--;
-                return;
-            }
+        {
+            System.out.println("Resume" + uuid + "There is not");
         }
-        System.out.println("Resume" + uuid + " There is no have");
     }
 
-    Resume[] getAll() { // Моментальный возврат
+
+    Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, sizeOfResume);
     }
 
-    int size() {
-        return sizeOfResume;
-    }
 }
-
